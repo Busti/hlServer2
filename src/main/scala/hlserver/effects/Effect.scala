cb8e6dea7d7d20aa937c575acbc75bc6e54a709d
Busti <mbust@live.de>
2017-09-29 08:02:33 +0200

    Simplified .gitignore

--- src/main/scala/hlserver/effects/Effect.scala
+++ src/main/scala/hlserver/effects/Effect.scala
@@ -3,17 +3,13 @@ package hlserver.effects
 import hlserver.util.{Color, Strip}
 import org.joda.time.DateTime
 
-import scala.annotation.StaticAnnotation
-
-import reflect.runtime.universe._
-
 trait Effect {
   def render(index: Int, strip: Strip, time: DateTime): Color
 }
 
 object Effect {
   def discover() {
-    typeOf[Effect].foreach(print(_))
+
   }
 }
 
